8196da1161972d8a3c562b19b2a83aa09cbfe74c
gmoccand <gael.moccand@gmail.com>
2021-08-08 13:12:26 +0200

    create class gNlpApi to wrapp API

---
+++ gCloudNlp.py
@@ -0,0 +1,157 @@
+# Imports the Google Cloud client library
+from pandas.core.indexes.base import ensure_index
+from google.cloud import language_v1
+import sys
+import pandas as pd
+
+
+
+class gNlpApi:
+    """
+    Class to wrap google Natural Language API
+
+    Args:
+      inputText The text content to analyze
+    """
+
+
+
+    def __init__(self, text, lang):
+        if len(text) < 20:
+            print("Text is too short. It must be at least 20 characters")
+            sys.exit()
+        else:
+            self.input_txt = text
+            self.input_lang = lang
+            self.client = language_v1.LanguageServiceClient()
+
+
+
+
+    def analyze_entity_sentiment(self):
+        """
+        Analyzing Entity Sentiment in a String
+
+        Args:
+        text_content The text content to analyze
+        """
+
+        res = {
+            'Name': [],
+            'Type': [],
+            'Salience': [],
+            'Score': [],
+            'Magnitude': [],
+            'Metadatas': [],
+        }
+
+        type_ = language_v1.Document.Type.PLAIN_TEXT
+
+        document = {"content": self.input_txt, "type_": type_, "language": self.input_lang}
+
+        # Available values: NONE, UTF8, UTF16, UTF32
+        encoding_type = language_v1.EncodingType.UTF8
+
+        response = self.client.analyze_entity_sentiment(
+            request={'document': document, 'encoding_type': encoding_type})
+
+        print(u"Sentiment Entities:\n")
+        # Loop through entitites returned from the API
+        for entity in response.entities:
+            print(u"Representative name for the entity: {}".format(entity.name))
+            res['Name'].append(format(entity.name))
+            # Get entity type, e.g. PERSON, LOCATION, ADDRESS, NUMBER, et al
+            ent = language_v1.Entity.Type(entity.type_).name
+            print(u"Entity type: {}".format(ent))
+            res['Type'].append(ent)
+
+            # Get the salience score associated with the entity in the [0, 1.0] range
+            print(u"Salience score: {}".format(entity.salience))
+            res['Salience'].append((entity.salience))
+
+            # Get the aggregate sentiment expressed for this entity in the provided document.
+            sentiment = entity.sentiment
+            print(u"Entity sentiment score: {}".format(sentiment.score))
+            res['Score'].append((sentiment.score))
+            print(u"Entity sentiment magnitude: {}".format(sentiment.magnitude))
+            res['Magnitude'].append((sentiment.magnitude))
+            # Loop over the metadata associated with entity. For many known entities,
+            # the metadata is a Wikipedia URL (wikipedia_url) and Knowledge Graph MID (mid).
+            # Some entity types may have additional metadata, e.g. ADDRESS entities
+            # may have metadata for the address street_name, postal_code, et al.
+            metadatas = str()
+            for metadata_name, metadata_value in entity.metadata.items():
+                print(u"{} = {}".format(metadata_name, metadata_value))
+                metadatas += u" {} = {}".format(metadata_name, metadata_value)
+
+            res['Metadatas'].append(metadatas)
+
+
+            # Loop over the mentions of this entity in the input document.
+            # The API currently supports proper noun mentions.
+            for mention in entity.mentions:
+                print(u"Mention text: {}".format(mention.text.content))
+                # Get the mention type, e.g. PROPER for proper noun
+                print(u"Mention type: {}".format(language_v1.EntityMention.Type(mention.type_).name))
+
+            print(u"\n")
+
+        # Get the language of the text, which will be the same as
+        # the language specified in the request or, if not specified,
+        # the automatically-detected language.
+        print(u"Language of the text: {}".format(response.language))
+        df = pd.DataFrame(res)
+        return df
+
+
+    def classify_text(self):
+        """
+        Classifying Content in a String
+
+        Args:
+        text_content The text content to analyze. Must include at least 20 words.
+        """
+
+        type_ = language_v1.Document.Type.PLAIN_TEXT
+
+        document = {"content": self.input_txt, "type_": type_, "language": self.input_lang}
+
+        res = {
+            'Name': [],
+            'Confindence': [],
+        }
+
+        response = self.client.classify_text(request={'document': document})
+        # Loop through classified categories returned from the API
+        for category in response.categories:
+            # Get the name of the category representing the document.
+            # See the predefined taxonomy of categories:
+            # https://cloud.google.com/natural-language/docs/categories
+            print(u"Category name: {} confidence {}".format(category.name, category.confidence))
+            # Get the confidence. Number representing how certain the classifier
+            # is that this category represents the provided text.
+            res['Name'].append((category.name))
+            res['Confindence'].append((category.confidence))
+
+        df = pd.DataFrame(res)
+        return df
+
+
+    def analyze_text_sentiment(self):
+        document = language_v1.Document(
+            content=self.input_txt, type_=language_v1.Document.Type.PLAIN_TEXT)
+
+        res = {
+            'Score': [],
+            'Magnitude': [],
+        }
+
+        # Detects the sentiment of the text
+        sentiment = self.client.analyze_sentiment(request={'document': document}).document_sentiment
+        print("whole Text Sentiment: {}, magnitude: {}".format(sentiment.score, sentiment.magnitude))
+
+        res['Score'].append((sentiment.score))
+        res['Magnitude'].append((sentiment.magnitude))
+        df = pd.DataFrame(res)
+        return df
+
--- getEntity.py
+++ getEntity.py
@@ -1,58 +1,44 @@
-from __future__ import print_function
-import json
-import urllib
-import urllib.parse
-from urllib.request import Request, urlopen
-import os
+# Imports the Google Cloud client library
+from pandas.core.indexes.base import ensure_index
+from google.cloud import language_v1
 import sys
-import datetime
-from collections import OrderedDict
-import time
-import shutil
-import time
-from inspect import getsourcefile
-from os.path import abspath
-
-#https://developers.google.com/knowledge-graph
-
-# parsing argument for cmd line arg
-class ArgParser:
-
-    input_txt = str()
-    failed = False
-    def __init__(self):
-        if len(sys.argv) == 2:
-            self.input_txt = str(sys.argv.pop()) 
-        else:
-            print("Too few arguments")
-            print("Syntax:")
-            print("use getEntity.py  \"your input\" ")
-            self.failed = True
-            exit
-
+import os
+import pandas as pd
+import argparse
+from gCloudNlp import gNlpApi
 
 if __name__ == '__main__':
 
-    arg = ArgParser()
-    if not arg.failed:
-        api_key = os.environ.get('API_KEY')
-        print(api_key)
-        query = arg.input_txt
-        service_url = 'https://kgsearch.googleapis.com/v1/entities:search'
-        params = {
-            'query': query,
-            'key': api_key,
-            'limit': 3,
-            'indent': True,
-        }
-        url = service_url + '?' + urllib.parse.urlencode(params)
-        q = Request(url)
-        r = urlopen(q).read()
-        response = json.loads(r)
-        for element in response['itemListElement']:
-            print(element['result']['name'] + ' (' + str(element['resultScore']) + ')')
-            print(element['result']['description'])
-            print(element['result']['@type'])
-            print(element['result']['detailedDescription'])
-            print('\n')
-    
+    parser = argparse.ArgumentParser(description='Analyze Entities and Sentiment from text input file ')
+    parser.add_argument('InputFile', metavar='path', type=str, help='input text file')
+    parser.add_argument('Results', metavar='path', type=str, help='output result .xlsx file')
+    parser.add_argument('--lang', default='en', const='en', nargs='?', choices=['en', 'fr', 'de', 'it'],  help='optional specify language [en|fr|de|it]')
+
+
+    args = parser.parse_args()
+
+    input_path = args.InputFile
+    results_path = args.Results
+    input_lang = args.lang
+
+
+
+    if not os.path.isfile(input_path):
+        print('The input text file specified does not exist')
+        sys.exit()
+
+
+    with open(input_path) as file:
+        in_text = file.read()
+        print(in_text)
+
+        g_nlp = gNlpApi(in_text, input_lang)
+
+        df_category = g_nlp.classify_text()
+        df_entity = g_nlp.analyze_entity_sentiment()
+        df_txt_senti = g_nlp.analyze_text_sentiment()
+
+        with pd.ExcelWriter(results_path + '.xlsx') as writer:
+            df_entity.to_excel(writer, sheet_name='SentimentEntities')
+            df_category.to_excel(writer, sheet_name='Category')
+            df_txt_senti.to_excel(writer, sheet_name='SentimentText')
